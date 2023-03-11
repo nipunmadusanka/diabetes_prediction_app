8b842cab22a6a375cd5a6fdabe53c3722dab9b33
nipunmadusanka <nm3nipun@gmail.com>
2023-03-11 23:52:14 +0530

    added

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, url_for, abort
+import pickle as pk
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from werkzeug.exceptions import HTTPException
+
+app = Flask(__name__)
+
+model = pk.load(open('diabetes_predict.pkl','rb'))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    # feature = np.array(int_features)
+    # re_shape = feature.reshape(1,-1)
+    # sc = StandardScaler()
+    # std_data = sc.fit_transform(feature)
+
+    # data = (0,158,76,36,245,31.6,0.851,28)
+
+    np_array = np.array(int_features)
+    re_shape = np_array.reshape(1,-1)
+    # std_data = sc.fit_transform(re_shape)
+    rslt = model.predict(re_shape)
+
+    if rslt[0] == 1:
+        text = "I think you have Diabets, Please meet you Doctor soon as posible"
+    else:
+        text = "Great, You haven't diabetes"
+    
+    return render_template('index.html', prediction_text='Result: {}'.format(text),
+                           text=': {}'.format(rslt) )
+
+@app.route('/simulate500')
+def simulate500():
+    return abort(500)
+
+@app.errorhandler(HTTPException)
+def errorhandler(err):
+    return render_template('index.html', title="Error", message="Ooops"), err.code
+
+if __name__ == "__main__":
+    app.run()
